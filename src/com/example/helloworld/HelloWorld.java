924116e7213b8d8af014f4a630a7528cf33baca7
Gouri <gourimahapatra@gmail.com>
2018-11-29 08:18:33 +0900

    first commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void  main(String[] args){
+        Integer x = 90;
+        System.out.println("Gouri");
+        System.out.println(x);
+        System.out.println("Gouri");
+    }
+}
